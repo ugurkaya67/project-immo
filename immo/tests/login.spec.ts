cc3f0bd8715fde682ebd87c9a76bcfad9014d487
Ugur <ugurkaya67@hotmail.com>
2026-03-17 17:27:00 +0100

    mise en place de Playwright et ajout du test de connexion admin

--- immo/tests/login.spec.ts
+++ immo/tests/login.spec.ts
@@ -1,16 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('connexion admin fonctionne', async ({ page }) => {
-  const email = process.env.ADMIN_EMAIL!;
-  const password = process.env.ADMIN_PASSWORD!;
-
-  console.log("ADMIN_EMAIL =", process.env.ADMIN_EMAIL);
-  console.log("ADMIN_PASSWORD =", process.env.ADMIN_PASSWORD);
+  //console.log("ADMIN_EMAIL =", process.env.ADMIN_EMAIL);
+  //console.log("ADMIN_PASSWORD =", process.env.ADMIN_PASSWORD);
 
   await page.goto('http://localhost:3000/login');
 
-  await page.fill('input[name="email"]', email);
-  await page.fill('input[name="password"]', password);
+  await page.fill('input[name="email"]', process.env.ADMIN_EMAIL!);
+  await page.fill('input[name="password"]', process.env.ADMIN_PASSWORD!);
 
   await Promise.all([
     page.waitForURL(/\/admin$/, {
